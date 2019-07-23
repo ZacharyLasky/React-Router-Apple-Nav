7a26f0445804c7c34c4daa4516ee42de5796da48
Zach Lasky <zacharylasky123@gmail.com>
2019-07-23 17:31:01 -0400

    added appleProducts object

--- apple-nav/src/NavFolder/Nav-Wrapper.js
+++ apple-nav/src/NavFolder/Nav-Wrapper.js
@@ -6,37 +6,37 @@ export default function NavWrapper() {
   return (
     <ul className="header">
         <li>
-          <NavLink exact to="/" activeClassName="activeNavButton">
+          <NavLink to="/" activeClassName="activeNavButton">
             Mac
           </NavLink>
         </li>
         <li>
-          <NavLink exact to="/iPad" activeClassName="activeNavButton">
+          <NavLink to="/iPad" activeClassName="activeNavButton">
             iPad
           </NavLink>
         </li>
          <li>
-          <NavLink exact to="/iPhone" activeClassName="activeNavButton">
+          <NavLink to="/iPhone" activeClassName="activeNavButton">
             iPhone
           </NavLink>
         </li>
          <li>
-          <NavLink exact to="/Watch" activeClassName="activeNavButton">
+          <NavLink to="/Watch" activeClassName="activeNavButton">
             Watch
           </NavLink>
         </li>
          <li>
-          <NavLink exact to="/TV" activeClassName="activeNavButton">
+          <NavLink to="/TV" activeClassName="activeNavButton">
             TV
           </NavLink>
         </li>
          <li>
-          <NavLink exact to="/Music" activeClassName="activeNavButton">
+          <NavLink to="/Music" activeClassName="activeNavButton">
             Music
           </NavLink>
         </li>
          <li>
-          <NavLink exact to="/Support" activeClassName="activeNavButton">
+          <NavLink to="/Support" activeClassName="activeNavButton">
             Support
           </NavLink>
         </li>
